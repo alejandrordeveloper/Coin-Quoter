0d59f502ac55fb0c17259c2b8ecd26521efc545b
manuel333m <manumendozao.602@gmail.com>
2025-07-21 16:18:07 -0400

    New

--- index.js
+++ index.js
@@ -4,9 +4,24 @@ const coin = document.querySelector("#coin");
 const crypto = document.querySelector("#crypto");
 const amount = document.querySelector("#amount");
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    const coinSelected = [...coin.children].find(Option => Option.selected);
-    
-})
-
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const coinSelected = [...coin.children].find(
+    (Option) => Option.selected
+  ).value;
+  const cryptoSelected = [...crypto.children].find(
+    (Option) => Option.selected
+  ).value;
+  const amountValue = amount.value;
+  // el fetch devulve una respuesta
+  try {
+    const response = await (
+      await fetch(
+        `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cryptoSelected}&tsyms=${coinSelected}`
+      )
+    ).json();
+    console.log(response.DISPLAY[cryptoSelected][coinSelected]);
+  } catch (error) {
+    console.log(error);
+  }
+});
