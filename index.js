54d9c56259cfd6a09c8249a2a74d765d47049d41
Alejandro Ramirez <alejandrocohete@gmail.com>
2025-08-04 21:56:27 -0400

    New

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const form = document.querySelector("#coin-form");
 const coin = document.querySelector("#coin");
 const crypto = document.querySelector("#crypto");
 const amount = document.querySelector("#amount");
+const coinInfo = document.querySelector("#coin-info");
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
@@ -15,12 +16,37 @@ form.addEventListener("submit", async (e) => {
   const amountValue = amount.value;
   // el fetch devulve una respuesta
   try {
+    coinInfo.innerHTML = `<div class="loader"></div>`;
     const response = await (
       await fetch(
         `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cryptoSelected}&tsyms=${coinSelected}`
       )
     ).json();
-    console.log(response.DISPLAY[cryptoSelected][coinSelected]);
+    const price = (response.DISPLAY[cryptoSelected][coinSelected].PRICE);
+    const highPrice = (response.DISPLAY[cryptoSelected][coinSelected].HIGH24HOUR);
+    const lowPrice = (response.DISPLAY[cryptoSelected][coinSelected].LOW24HOUR);
+    const variation = (response.DISPLAY[cryptoSelected][coinSelected].CHANGEPCT24HOUR);
+    console.log(response.RAW[cryptoSelected][coinSelected].PRICE);
+    
+
+    if (amountValue !== "" ) {
+      const result = Number(amountValue) / response.RAW[cryptoSelected][coinSelected].PRICE;
+      coinInfo.innerHTML = `
+            <p class="info">El precio es: <span class="price">${price}</span></p>
+            <p class="info">El precio mas alto es: <span class="price">${highPrice}</span></p>
+            <p class="info">El precio mas bajo es: <span class="price">${lowPrice}</span></p>
+            <p class="info">Variacion 24H: <span class="price">${variation}</span></p>
+            <p class="info">Puede Comprar: <span class="price">${result.toFixed(4)} ${cryptoSelected}</span></p>`;
+      
+    } else {
+      coinInfo.innerHTML = `
+            <p class="info">El precio es: <span class="price">${price}</span></p>
+            <p class="info">El precio mas alto es: <span class="price">${highPrice}</span></p>
+            <p class="info">El precio mas bajo es: <span class="price">${lowPrice}</span></p>
+            <p class="info">Variacion 24H: <span class="price">${variation}</span></p>
+           `;
+    }
+      
   } catch (error) {
     console.log(error);
   }
